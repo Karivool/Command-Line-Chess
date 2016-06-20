a58532befb72e4bdc5356df82a56226de217c630
Evelyn Lee <evelynlee34@gmail.com>
2016-06-20 17:18:43 -0400

    add piece symbols

--- Chess/piece.rb
+++ Chess/piece.rb
@@ -1,10 +1,16 @@
 class Piece
-  attr_reader :color, :board, :position
+  attr_reader :color, :board, :position, :symbol
 
   def initialize(color, board, position)
     @color = color
     @board = board
     @position = position
+    @symbol = "X"
+  end
+
+  def symbol
+    @symbol.colorize(self.color)
+    # print " ".colorize(:background => :green)
   end
 
   def valid_moves
