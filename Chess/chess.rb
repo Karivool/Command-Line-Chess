6de032d3921b7341b686ffd941acded66356d549
Evelyn Lee <evelynlee34@gmail.com>
2016-06-21 11:11:23 -0400

    add move logic

--- Chess/chess.rb
+++ Chess/chess.rb
@@ -1,4 +1,5 @@
-
+require_relative 'display.rb'
+require_relative 'board.rb'
 
 class Chess
   attr_reader :board, :display
@@ -14,18 +15,27 @@ class Chess
     end
   end
 
+  def won?
+    false
+  end
+
   def play_turn
     start_position = get_position
     end_position = get_position
-    move(start_position, end_position)
+    @board.move(start_position, end_position)
   end
 
   def get_position
-
+    until false
+      @display.render
+      break unless @display.get_input.nil?
+    end
+    @display.cursor
   end
 
-  def move
+end
 
-  end
-
+if __FILE__ == $PROGRAM_NAME
+  c = Chess.new
+  c.play
 end
